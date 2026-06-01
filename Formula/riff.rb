c74b1f2f2abcdb8f8cc2adf48194cdbc473216c1
Caleb Cauthon <calebcauthon@gmail.com>
2026-06-01 00:04:59 -0500

    release: v0.1.3

--- Formula/riff.rb
+++ Formula/riff.rb
@@ -1,8 +1,8 @@
 class Riff < Formula
   desc "Local-first voice notes and transcription CLI"
   homepage "https://github.com/calebcauthon/riff"
-  url "https://github.com/calebcauthon/riff/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/calebcauthon/riff/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "3111f1e4f833993364405bc5e1937e319400aa6f3c69026e64e02fd2952dbbfa"
   head "https://github.com/calebcauthon/riff.git", branch: "main"
 
   depends_on "rust" => :build
