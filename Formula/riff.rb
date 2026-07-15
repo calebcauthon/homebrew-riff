418840917d0263bb603c3470302111b358447d5b
Caleb Cauthon <calebcauthon@gmail.com>
2026-07-15 12:30:56 -0500

    fix riff formula resources

--- Formula/riff.rb
+++ Formula/riff.rb
@@ -6,12 +6,16 @@ class Riff < Formula
   head "https://github.com/calebcauthon/riff.git", branch: "main"
 
   depends_on "rust" => :build
+  depends_on "ffmpeg"
+  depends_on "python@3.12"
 
   def install
     system "cargo", "install", *std_cargo_args(path: ".")
+    libexec.install "scripts"
   end
 
   test do
     assert_match "riff ", shell_output("#{bin}/riff --version")
+    assert_match "parakeet_script", shell_output("#{bin}/riff doctor")
   end
 end
